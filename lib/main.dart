90951a4a4295d6df747eb4e6345423fd398f5ddd
davidaskenazy <davidask.da@gmail.com>
2020-10-19 17:26:01 -0300

    BottomNavigationBar Added !

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:tripsApp/description_place.dart';
-import 'package:tripsApp/review_list.dart';
-import 'header_appbar.dart';
+import 'tripsApp.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  String descriptionDummy =
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,34 +26,12 @@ class MyApp extends StatelessWidget {
           // closer together (more dense) than on mobile platforms.
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: Scaffold(
-          // body: new DescriptionPlace("Bahamas", 4, descriptionDummy),
-          body: Stack(
-            children: <Widget>[
-              ListView(
-                children: <Widget>[
-                  DescriptionPlace("Bahamas", 4, descriptionDummy),
-                  ReviewList()
-                ],
-              ),
-              HeaderAppBar()
-            ],
-          ),
-        ));
+        home: tripsApp());
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -70,32 +44,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
---
+++ lib/tripsApp.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'home_trips.dart';
+import 'search_trips.dart';
+import 'profile_trips.dart';
+
+class tripsApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _tripsApp();
+  }
+}
+
+class _tripsApp extends State<tripsApp> {
+  int indexTap = 0;
+
+  final List<Widget> widgetsChildren = [
+    HomeTrips(),
+    SearchTrips(),
+    ProfileTrips()
+  ];
+
+  void onTapTapped(int index) {
+    setState(() {
+      indexTap = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // body: new DescriptionPlace("Bahamas", 4, descriptionDummy),
+      body: widgetsChildren[indexTap],
+      bottomNavigationBar: Theme(
+          data: Theme.of(context)
+              .copyWith(canvasColor: Colors.white, primaryColor: Colors.purple),
+          child: BottomNavigationBar(
+              onTap: onTapTapped,
+              currentIndex: indexTap,
+              items: [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.home), title: Text("")),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.search), title: Text("")),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.person), title: Text("")),
+              ])),
+    );
+  }
+}
